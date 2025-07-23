8290f535df6f5e30a48f988d6c9b8b3175c2039c
Ishan Ahluwalia <ishanahluwalia@Ishans-MacBook-Pro-2.local>
2025-07-22 17:35:51 -0700

    Update app to use trained model

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
+from fastai.vision.all import *
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!"
+# Load the trained model
+learn = load_learner("animal_classifier.pkl")
+labels = learn.dls.vocab
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+def classify(img):
+    pred, idx, probs = learn.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+
+demo = gr.Interface(
+    fn=classify,
+    inputs=gr.Image(type="pil"),
+    outputs=gr.Label(num_top_classes=3),
+    title="🐾 Animal Species Classifier",
+    description="Upload an animal photo – the model returns its top guesses."
+)
+
 demo.launch()
-
 
 
 
